374a0cf5edc66d2fb99cea8ddbd804c1915801de
kucira <kucira@users.noreply.github.com>
2023-03-11 00:00:21 +0700

    5 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import solidJs from "@astrojs/solid-js";
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), solidJs(), preact()]
+  integrations: [mdx(), sitemap(), solidJs(), preact(), tailwind()]
 });
