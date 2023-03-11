78b15dca8c14a85fa648e0e7591ab9f861cb1f62
kucira <kucira@users.noreply.github.com>
2023-03-11 12:31:32 +0700

    2 files modified

--- src/components/PreactComponent.jsx
+++ src/components/PreactComponent.jsx
@@ -2,10 +2,22 @@ import { useState } from 'preact/hooks';
 
 const PreactComponent = () => {
   const [count, setCount] = useState(0);
+
+  function foo() {
+    console.log(this, 'oke');
+  }
+
   return (
     <div>
       <h1>Preact Component {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          console.log(new foo());
+        }}
+      >
+        Increment
+      </button>
     </div>
   );
 };
